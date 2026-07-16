da4397079a42529a04e1aec9a256390b7df11485
Tanveer Ahmad <jamtanveerab@gmail.com>
2026-07-16 19:34:18 +0500

    Make it responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,9 +18,9 @@ const Navbar = (props) => {
     <nav className="navbar navbar-expand-lg bg-white border-bottom py-2 ">
       <div className="container-fluid container">
         <i className="fa-solid fa-book mx-4"></i>
-        <Link className="navbar-brand" to="/">iNoteBook</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon d-none"></span>
+        <Link className="navbar-brand fixed" to="/">iNoteBook</Link>
+        <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon "></span>
         </button>
         <div className="collapse navbar-collapse d-flex justify-content-end mx-4" id="navbarSupportedContent">
           {localStorage.getItem('token') ? <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -31,10 +31,11 @@ const Navbar = (props) => {
               <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
             </li>
           </ul> : ""}
-          {!localStorage.getItem('token') ? <form className="d-flex">
-            <button type="button" className="btn hoovr" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
-            <button type="button" className="btn btn-primary mx-1 rounded-3" data-bs-toggle="modal" data-bs-target="#signupModal">Get Started</button>
-          </form> : <button className='btn btn-primary' onClick={handleLogout}> Logout</button>}
+          {!localStorage.getItem('token') ? <div className="w-100 d-flex justify-content-center mt-2 justify-content-lg-end">
+            <form className="d-flex">
+              <button type="button"className="btn hoovr" data-bs-toggle="modal" data-bs-target="#loginModal"> Login</button>
+              <button type="button"className="btn btn-primary ms-2 rounded-3" data-bs-toggle="modal" data-bs-target="#signupModal">Get Started</button> </form>
+          </div> : <button className='btn btn-primary' onClick={handleLogout}> Logout</button>}
         </div>
       </div>
       <LoginModal showAlert={props.showAlert} />
